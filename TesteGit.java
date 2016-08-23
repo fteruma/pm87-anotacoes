8cee8010663f0398f748b5856c885936e5924fc8
Faruk Teruma <Faruk Teruma>
2016-08-23 23:31:50 +0200

    Adicionamos Reticencias

--- TesteGit.java
+++ TesteGit.java
@@ -1,2 +1,5 @@
-			public	class	TesteGit{													public	static	void main(String[]	args) {																	
-System.out.println("Exemplo	de	teste	de	uso	do	git");													}									}
+public class TesteGit{
+	public static void main(String[] args){
+		System.out.println("Exemplo de teste de	uso do git ....");
+	}
+}
